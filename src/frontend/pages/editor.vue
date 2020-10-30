21601fb7d2375cccec7dcced180f964de8717198
tabjy <admin@tabjy.com>
2020-10-30 01:35:45 -0700

    Implement in-browser code editor

--- src/frontend/pages/editor.vue
+++ src/frontend/pages/editor.vue
@@ -1,14 +1,28 @@
 <template>
   <v-container fill-height style="max-width: none; padding: 0; display: block; ">
-    <v-row style="height: 40px; background-color: lightgrey" />
+    <v-row style="height: 40px; background-color: lightgrey"/>
     <v-row style="height: calc(100% - 80px)">
-      <v-col cols="4" style="background-color: grey" />
+      <v-col cols="4" style="background-color: grey"/>
 
       <v-col cols="8" style="background-color: white">
-        <div ref="editor" style="height: 100%; width: 100%" />
+        <iframe
+          v-show="loaded"
+          id="editor-iframe"
+          src="/editor-iframe.html"
+          style="height: 100%; width: 100%; border: none;"
+        />
+        <v-row v-show="!loaded" align="center" justify="center" style="height: 100%">
+          <div class="text-center">
+            <v-progress-circular
+              indeterminate
+              color="primary"
+            />
+            <p>Loading code editor...</p>
+          </div>
+        </v-row>
       </v-col>
     </v-row>
-    <v-row style="height: 40px; background-color: lightgrey" />
+    <v-row style="height: 40px; background-color: lightgrey"/>
   </v-container>
 </template>
 
@@ -19,7 +33,44 @@ html {
 </style>
 
 <script>
-export default {
+const source = `#include <stdio.h>
 
+int main() {
+   printf("Hello, World!");
+   return 0;
+}
+`
+
+export default {
+  data () {
+    return {
+      loaded: false
+    }
+  },
+  mounted () {
+    const editor = document.getElementById('editor-iframe')
+
+    window.onmessage = (e) => {
+      let data
+      try {
+        data = JSON.parse(e.data)
+      } catch (err) {
+        return
+      }
+
+      switch (data.method) {
+        case 'gotValue':
+          console.log(data.value)
+          return
+        case 'ready':
+          this.loaded = true
+          editor.contentWindow.postMessage(JSON.stringify({
+            method: 'createEditor',
+            value: source,
+            language: 'cpp'
+          }), '*')
+      }
+    }
+  }
 }
 </script>
