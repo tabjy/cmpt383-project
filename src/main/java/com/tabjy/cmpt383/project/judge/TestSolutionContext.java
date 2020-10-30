30d98a8b9280b64a3afcfda0da2f2c05bfc71415
tabjy <admin@tabjy.com>
2020-10-29 21:18:04 -0700

    Support nodejs runner

--- src/main/java/com/tabjy/cmpt383/project/judge/TestSolutionContext.java
+++ src/main/java/com/tabjy/cmpt383/project/judge/TestSolutionContext.java
@@ -1,22 +1,23 @@
 package com.tabjy.cmpt383.project.judge;
 
-import com.tabjy.cmpt383.project.judge.builder.InterpretedLanguageBuildStrategy;
-import com.tabjy.cmpt383.project.judge.runner.NativeOnHostRunStrategy;
-import com.tabjy.cmpt383.project.judge.runner.NativeRunStrategy;
+import com.tabjy.cmpt383.project.judge.builder.BuildStrategies;
+import com.tabjy.cmpt383.project.judge.runner.RunStrategies;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class TestSolutionContext {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, LanguageNotSupportedException {
         SolutionContext ctx = new SolutionContext();
-        ctx.setBuildStrategy(new InterpretedLanguageBuildStrategy());
-//        ctx.setRunStrategy(new NativeOnHostRunStrategy());
-        ctx.setRunStrategy(new NativeRunStrategy());
+        ctx.setBuildStrategy(BuildStrategies.forLanguage("javascript"));
+        ctx.setRunStrategy(RunStrategies.forLanguage("javascript"));
 
-        ctx.addSourceFile("main", "#!/usr/bin/bash\necho hello".getBytes(StandardCharsets.UTF_8));
-        ctx.build();
-        ExecResult result = ctx.run("main", new String[0]);
+        ctx.addSourceFile("main.js", "console.log('hello');console.error('world');process.exit(2);".getBytes(StandardCharsets.UTF_8));
+        ExecResult result = ctx.build();
+        if (result.exitCode != 0) {
+            System.err.println(result);
+        }
+        result = ctx.run("main.js", new String[0]);
         System.out.println(result);
     }
 }
---
+++ src/main/java/com/tabjy/cmpt383/project/judge/runner/RunStrategies.java
@@ -0,0 +1,32 @@
+package com.tabjy.cmpt383.project.judge.runner;
+
+import com.tabjy.cmpt383.project.judge.LanguageNotSupportedException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+public class RunStrategies {
+    private static final Map<String, Class<? extends IRunStrategy>> STRATEGY_MAP;
+
+    static {
+        STRATEGY_MAP = Map.of( //
+                "c", NativeRunStrategy.class, //
+                "c++", NativeRunStrategy.class, //
+                "go", NativeRunStrategy.class, //
+                "javascript", NodejsRunStrategy.class //
+        );
+    }
+
+    public static IRunStrategy forLanguage(String language) throws LanguageNotSupportedException {
+        Class<? extends IRunStrategy> strategy = STRATEGY_MAP.get(language.toLowerCase());
+        if (strategy == null) {
+            throw new LanguageNotSupportedException(language);
+        }
+
+        try {
+            return strategy.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
